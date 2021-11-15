5d18d7950224c12de5550f0946571f37cd61a279
Kyle Mitchell <kdavidmitchell@gmail.com>
2021-11-15 15:05:07 -0800

    adding base game layer scripts

---
+++ oops-all-bards/Assets/Scripts/Items/BaseItem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BaseItem
+{
+    private string name { get; set; }
+    private string description { get; set; }
+    private ItemTypes type { get; set; }
+    private int value { get; set; }
+
+    public enum ItemTypes
+    {
+        DUDS,
+        INSTRUMENTS,
+        POTABLES
+    }
+}
---
+++ oops-all-bards/Assets/Scripts/Stats/BaseStat.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BaseStat
+{
+    private string name { get; set; }
+    private string description { get; set; }
+    private StatTypes type { get; set; }
+    private int baseValue { get; set; }
+    private int modifiedValue { get; set; }
+
+    public enum StatTypes
+    {
+        FLOURISH,
+        ORATORY,
+        RHETORIC,
+        TEMPO,
+        ELAN
+    }
+}
