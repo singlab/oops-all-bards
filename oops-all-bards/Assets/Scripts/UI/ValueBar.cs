d8490a1451195df33d8e00faf39dda6428fd9d94
kdavidmitchell <kdavidmitchell@gmail.com>
2022-02-22 12:09:11 -0800

    combat ui changes

---
+++ oops-all-bards/Assets/Scripts/UI/ValueBar.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ValueBar : MonoBehaviour
+{
+    // References to the text and fill components of the value bar.
+    private TMP_Text textComponent;
+    private Image fillComponent;
+    // References to max value.
+    public int maxValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComponent = gameObject.GetComponentInChildren<TMP_Text>();
+        fillComponent = gameObject.transform.GetChild(0).GetComponentInChildren<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Update value bar with new value to display
+    public void UpdateValueBar(int newValue)
+    {
+        textComponent.text = newValue.ToString() + " / " + maxValue;
+        fillComponent.GetComponent<RectTransform>().localScale = new Vector3((newValue/maxValue), 1, 1);
+    }
+}
