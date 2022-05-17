9468ff9f1b810096ea60278487bdaa81d6c1d6d1
kdavidmitchell <kdavidmitchell@gmail.com>
2022-05-17 12:05:50 -0700

    adding animations and controllers for player and npcs

--- oops-all-bards/Assets/Scripts/Player/PlayerController.cs
+++ oops-all-bards/Assets/Scripts/Player/PlayerController.cs
@@ -5,17 +5,31 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private GameObject player;
+    private Animator animator;
+    private Vector3 forward;
+    private Vector3 right;
     public float speed = 5;
 
     void Start()
     {
     	player = gameObject;
+        animator = player.GetComponent<Animator>();
     }
  
     void Update()
     {
-        Vector3 forward = player.transform.forward;
-        Vector3 right = player.transform.right;
+        // Update forward/right for rotation of player
+        forward = player.transform.forward;
+        right = player.transform.right;
+
+        //Handle animation parameters
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))
+        {
+            animator.SetBool("isMoving", true);
+        } else
+        {
+            animator.SetBool("isMoving", false);
+        }
 
         // Move Forwards
         if (Input.GetKey(KeyCode.W)) 
