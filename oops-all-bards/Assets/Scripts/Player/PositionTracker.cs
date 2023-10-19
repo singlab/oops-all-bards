e74bb2c910982aadfe7340bd9e1c8510e4c37b40
Fu-Hsin Liao <s82522658@yahoo.com.tw>
2023-10-18 17:23:53 -0700

    Add a comment
    Todo: PositionTrackers only tracks NPC that has been recruited, but the system are supposed to track every NPC. We need to create an NPC manager that tracks all NPCs that use WanderNPCAI.cs

--- oops-all-bards/Assets/Scripts/Player/PositionTracker.cs
+++ oops-all-bards/Assets/Scripts/Player/PositionTracker.cs
@@ -15,6 +15,8 @@ public class PositionTracker
     public void updatePosition(Vector3 pos)
     {
         if(player == null){
+            //Todo: Now only tracks NPC that has been recruited, but the system are supposed to track every NPC.
+            //Need to create an NPC manager that tracks all NPCs which use WanderNPCAI.cs
             player = PartyManager.Instance.FindPartyMemberByName(name);
         }
         if(player != null){
