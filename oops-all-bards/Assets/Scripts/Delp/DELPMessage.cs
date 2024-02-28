c3a0365c7689a8b54db99334a4432abf71077589
kdavidmitchell <kdavidmitchell@gmail.com>
2024-02-28 15:18:48 -0800

    rework

--- oops-all-bards/Assets/Scripts/Delp/DELPMessage.cs
+++ oops-all-bards/Assets/Scripts/Delp/DELPMessage.cs
@@ -2,13 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DELPTest : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        // DelpHandler handler = new DelpHandler();   
-        // handler.empty();
+        
     }
 
     // Update is called once per frame
